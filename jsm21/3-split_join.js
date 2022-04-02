7e09a17e1916561951608b3a368549e3877eb016
Ihor Andrieiev <airxapko@gmail.com>
2022-04-02 17:11:05 +0300

    start

---
+++ jsm21/3-split_join.js
@@ -0,0 +1,65 @@
+// const str = 'Keep calm and carry on';
+// // const str = 'Keep|calm|and|carry|on';
+
+// // const delimeter = 'a';
+
+// const connector = '|';
+
+// const arrFromStr = str.split(' ');
+
+// const strFromArray = arrFromStr.join();
+
+// // console.log(str);
+// console.log(arrFromStr);
+// console.log(strFromArray);
+
+////////////////////////////////////////////////
+
+// Напиши скрипт для обчислення площі прямокутника зі сторонами, значення яких зберігаються в змінній величині у вигляді рядка.
+// Значення гарантовано розділені пробілом.
+
+// const values = '8 11';
+
+// const arr = values.split(' ');
+
+// const square = Number(arr[0]) * Number(arr[1]);
+
+// console.log(arr);
+// console.log(square);
+
+//////////////////////////////////////////////////////////
+
+// const password = '    5   5     ';
+// const passwordNorm = '    5   5     '.trim();
+
+// console.log(password);
+// console.log(passwordNorm);
+
+// if (password.length > 7) {
+//   console.log('OK');
+// }
+
+////////////////////////////////////////////////////////////
+
+// reverse (мутуючий метод)
+
+// const arr = [1, 5, 9, 8, 7, 10];
+
+// console.log(arr);
+
+// const newArr = arr.reverse();
+
+// console.log(arr);
+// console.log(newArr);
+
+//////////////////////////////////////////////////
+
+// palindrome
+
+// // Перевірити, чи є слово паліндромом
+// const word = 'потоп';
+// // const word = 'ропот';
+
+// const reversedStr = word.split('').reverse().join('');
+
+// console.log(word === reversedStr);
---
+++ jsm21/6-slice.js
@@ -0,0 +1,32 @@
+// slice
+// start index and end index (not included)
+
+// const arr = [5, 10, 12, 15, 25];
+
+// const arrCopy = arr.slice(-3, -2);
+
+// console.log(arr === arrCopy);
+
+// console.log(arr);
+// console.log(arrCopy);
+
+///////////////// Copy
+
+// const arr = [5, 10, 12, 15, 25];
+// const arrCopy = arr.slice();
+
+// arr[0] = 100;
+
+// console.log(arr);
+// console.log(arrCopy);
+// console.log(arr === arrCopy);
+
+/////////////////////////////////////////////////////
+
+// Є масив із парною кількістю елементів
+// Потрібно поділити його на дві половини
+
+// const numbers = [5, 10, 20, 35, 55, 80, 95, 110, 10];
+
+// console.log('~ firstHalf', firstHalf);
+// console.log('~ secondHalf', secondHalf);
